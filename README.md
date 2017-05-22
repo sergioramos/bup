a349e69b1d7549479b7dafc5f75b080068a51df6
Sérgio Ramos <mail@sergioramos.me>
2017-05-22 13:08:22 +0100

    initial implementation

---
+++ README.md
@@ -0,0 +1,29 @@
+# bup
+
+[![license](https://img.shields.io/github/license/ramitos/bup.svg?style=flat-square)](https://npmjs.com/package/bup)
+[![npm](https://img.shields.io/npm/v/bup.svg?style=flat-square)](https://npmjs.com/package/bup)
+[![standard-readme compliant](https://img.shields.io/badge/standard--readme-OK-green.svg?style=flat-square)](https://github.com/RichardLitt/standard-readme)
+
+Automated [rollup](https://github.com/rollup/rollup) build based on [babel](https://github.com/rollup/rollup-plugin-babel).
+
+## Table of Contents
+
+- [Install](#install)
+- [Usage](#usage)
+- [License](#license)
+
+## Install
+
+```
+yarn add --dev bup
+```
+
+## Usage
+
+```bash
+bup . # compiles pkg.entry to dist/name.format.js
+```
+
+## License
+
+BSD-3-Clause
---
+++ bin/index.js
@@ -0,0 +1,167 @@
+#!/usr/bin/env node
+
+const babel = require('rollup-plugin-babel');
+const { default: babelrc } = require('babelrc-rollup');
+const { exists, stat, writeFile, readFile } = require('mz/fs');
+const paramCase = require('param-case');
+const path = require('path');
+const argv = require('yargs').argv;
+const forEach = require('apr-for-each');
+const parallel = require('apr-parallel');
+const main = require('apr-main');
+const readPkg = require('read-pkg');
+const pkgDir = require('pkg-dir');
+const { rollup } = require('rollup');
+const makeDir = require('make-dir');
+const uniq = require('lodash.uniq');
+const forceArray = require('force-array');
+
+const appendExternalHelpers = conf => {
+  conf.plugins = forceArray(conf.plugins);
+  conf.plugins = uniq(conf.plugins.concat(['external-helpers']));
+  return conf;
+};
+
+const babelConfig = async ({ root, pkg }) => {
+  const dotrc = path.join(root, '.babelrc');
+  const dotrcExists = await exists(dotrc);
+
+  if (dotrcExists) {
+    const str = await readFile(dotrc, 'utf-8');
+    return appendExternalHelpers(JSON.parse(str));
+  }
+
+  const dotjson = path.join(root, '.babelrc.json');
+  const dotjsonExists = await exists(dotjson);
+
+  if (dotjsonExists) {
+    const str = await readFile(dotjson, 'utf-8');
+    return appendExternalHelpers(JSON.parse(str));
+  }
+
+  return appendExternalHelpers(pkg.babel || {});
+};
+
+const build = async ({ pkg, main, external, root }) => {
+  const bundle = await rollup({
+    entry: main,
+    external: external,
+    plugins: [
+      babel(
+        babelrc({
+          config: await babelConfig({ root, pkg })
+        })
+      )
+    ]
+  });
+
+  const write = fmt => async () => {
+    const { code } = bundle.generate({
+      format: fmt,
+      moduleId: pkg.name,
+      moduleName: pkg.name,
+      sourceMap: 'inline'
+    });
+
+    const file = await dest({ root, main, fmt, pkg });
+
+    await makeDir(path.dirname(file));
+    return writeFile(file, code);
+  };
+
+  return parallel([write('umd'), write('es'), write('iife')]);
+};
+
+const dest = async ({ root, main, fmt, pkg }) => {
+  if (!pkg) {
+    return path.join(process.cwd(), `dist/index.${fmt}.js`);
+  }
+
+  if (!pkg.entry || path.resolve(pkg.entry) === path.resolve(main)) {
+    return path.join(root, `dist/${paramCase(pkg.name)}.${fmt}.js`);
+  }
+
+  const name = path.basename(main, path.extname(main));
+  return path.join(process.cwd(), `dist/${paramCase(name)}.${fmt}.js`);
+};
+
+const getPkg = async dir => {
+  if (!dir) {
+    return;
+  }
+
+  const pathanme = path.join(dir, 'package.json');
+  const hasPkg = await exists(pathanme);
+
+  if (!hasPkg) {
+    return;
+  }
+
+  return await readPkg(pathanme);
+};
+
+const pkgEntry = async location => {
+  const root = await pkgDir(location);
+  const pkg = await getPkg(root);
+  const pkgEntry = path.resolve(root, pkg.entry || 'index.js');
+  const pkgEntryExists = await exists(pkgEntry);
+  return pkgEntryExists ? pkgEntry : null;
+};
+
+const entry = async location => {
+  const isDir = (await stat(location)).isDirectory();
+
+  if (!isDir) {
+    return pathanme;
+  }
+
+  const index = path.join(location, 'index.js');
+  const indexExists = await exists(index);
+
+  if (indexExists) {
+    return index;
+  }
+
+  const main = await pkgEntry(location);
+
+  if (main) {
+    return main;
+  }
+
+  throw new Error("Can't resolve entrypoint");
+};
+
+const external = async ({ pkg }) => {
+  return ['dependencies', 'devDependencies', 'peerDependencies'].reduce(
+    (deps, name) => Object.keys(pkg[name] || {}).concat(deps),
+    []
+  );
+};
+
+const run = async () => {
+  if (!argv._.length) {
+    argv._.push('.');
+  }
+
+  return await forEach(argv._, async arg => {
+    const location = path.resolve(process.cwd(), arg);
+    const main = await entry(location);
+    const root = await pkgDir(main);
+    const pkg = await getPkg(root);
+
+    const ctx = {
+      main,
+      location,
+      root,
+      pkg
+    };
+
+    return build(
+      Object.assign(ctx, {
+        external: await external(ctx)
+      })
+    );
+  });
+};
+
+main(run());
