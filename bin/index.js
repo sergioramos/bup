59afcedd21d2fd1a6373ae2144fe569f79eef14a
Sérgio Ramos <mail@sergioramos.me>
2019-03-14 20:24:27 +0000

    fix missing dep

--- bin/index.js
+++ bin/index.js
@@ -6,7 +6,6 @@ const babel = require('rollup-plugin-babel');
 const nodeResolve = require('rollup-plugin-node-resolve');
 const commonjs = require('rollup-plugin-commonjs');
 const json = require('rollup-plugin-json');
-const sourceMaps = require('rollup-plugin-sourcemaps');
 
 const { exists, stat, writeFile, readFile } = require('mz/fs');
 const paramCase = require('param-case');
